f636875e7ebe08f0ae7c098dbb74c6aed6f645b1
V1cente12 <jvicentecv@gmail.com>
2024-07-16 15:53:25 -0400

    estructura de bd

---
+++ database/migrations/2024_07_15_033952_create_shifts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShiftsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shifts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('task_id')->constrained()->onDelete('cascade');
+            $table->timestamp('completed_at')->nullable();
+            $table->foreignId('validated_by')->nullable()->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shifts');
+    }
+}
