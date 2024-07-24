03b0c26f0e602665674d28afc450b2f4ef626a2c
V1cente12 <jvicentecv@gmail.com>
2024-07-23 23:59:37 -0400

    funcion eliminar task en task service

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -6,30 +6,25 @@ use App\Models\Task;
 
 class TaskService
 {
-    //buscar tarea por id
+    //buscar task por id
     public function find($id)
     {
         return Task::findOrFail($id);
     }
-    //listar todas las tareas
+
+    //listar todas las task
     public function all()
     {
         return Task::all();
     }
     
-    //crear tarea
+    //crear task
     public function create(array $data)
     {
         return Task::create($data);
     }
 
-    public function update($id, array $data)
-    {
-        $task = Task::findOrFail($id);
-        $task->update($data);
-        return $task;
-    }
-
+    //eliminar task
     public function delete($id)
     {
         $task = Task::findOrFail($id);
