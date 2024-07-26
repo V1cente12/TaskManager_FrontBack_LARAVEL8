cf767a692c4ce131560b03583304114c7a7dfc0a
V1cente12 <jvicentecv@gmail.com>
2024-07-25 22:01:02 -0400

    quitar comentarios en vista

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -9,17 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">     
-                    <!-- Botón con estilo -->
                     <form method="POST" action="{{ route('mark-task', ['task_id' => $task->id, 'user_id' => $user->id]) }}">
                         @csrf
-                        <!-- No es necesario usar campos ocultos si ya estás pasando los parámetros en la URL -->
                         <button type="submit" class="mt-4 px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
                             Marcar como Tarea realizada
                         </button>
                     </form>
-                    
-                    
-                    <!-- Tabla con estilos -->
+
                     <div class="mt-8 overflow-x-auto">
                         <table class="min-w-full bg-white">
                             <thead>
@@ -42,7 +38,6 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <!-- Aquí irán las filas de datos -->
                                 <tr>
                                     @foreach ($shifts as $shift)
                                     <tr>
@@ -71,7 +66,6 @@
                                                 <button @click="showPopup = true" class="px-4 py-2 bg-gray-500 text-white font-semibold rounded-lg shadow-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75">
                                                     Validar
                                                 </button>
-                                                <!-- Popup -->
                                                 <div x-show="showPopup" class="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-75" x-transition>
                                                     <div class="bg-white p-4 rounded-lg shadow-lg">
                                                         <h2 class="text-lg font-semibold">Este registro ya fue validado</h2>
@@ -82,7 +76,6 @@
                                                 </div>
                                             @endif
                                         </td>
-                                        
                                     </tr>
                                 @endforeach
                                 </tr>
