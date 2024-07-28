e8a89be752eb25950318a72091f3fec76e63f241
V1cente12 <jvicentecv@gmail.com>
2024-07-27 23:19:27 -0400

    role has permissions service

---
+++ app/Services/RoleHasPermissionsService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Role_Has_Permissions;
+
+class RoleHasPermissionsService
+{
+    //busca todas los registros
+    public function all()
+    {
+        return Role_Has_Permissions::all();
+    }
+}
