60e335d38f603c00c763ced9d732ae287bfeeb46
V1cente12 <jvicentecv@gmail.com>
2024-07-27 23:18:17 -0400

    correcion el role service

--- app/Services/RoleService.php
+++ app/Services/RoleService.php
@@ -6,14 +6,6 @@ use App\Models\Role;
 
 class RoleService
 {
-    //buscar registro por tarea
-    public function findRolbyId($user_id)
-    {
-        return Role::where('user_id', $user_id)
-            ->with(['user'])
-            ->get();
-    }
-
     //busca todas los registros
     public function all()
     {
