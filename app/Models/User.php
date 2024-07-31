7591e222f9219ff22fc42ad9d2223d3c71d1583a
Vicente Calderon <77591317+V1cente12@users.noreply.github.com>
2024-07-30 23:50:13 -0400

    Actualizar User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,8 +42,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
    
-    public function roles()
-    {
+    public function roles(){
         return $this->belongsToMany(Role::class, 'role_has_permissions', 'user_id', 'role_id');
     }
 }
