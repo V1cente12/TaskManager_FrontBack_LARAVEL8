cd5bfa08bebcb1a523d3e2f7832dca6f7976fd5d
Vicente Calderon <77591317+V1cente12@users.noreply.github.com>
2024-07-30 23:46:28 -0400

    Actualizar RoleHasPermissionsService.php

--- app/Services/RoleHasPermissionsService.php
+++ app/Services/RoleHasPermissionsService.php
@@ -7,8 +7,7 @@ use App\Models\Role_Has_Permissions;
 class RoleHasPermissionsService
 {
     //busca todas los registros
-    public function all()
-    {
+    public function all(){
         return Role_Has_Permissions::all();
     }
 }
