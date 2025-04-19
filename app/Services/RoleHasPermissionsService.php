2af96d2bb7c395206dfb5708fd68eed6a73d325a
V1cente12 <jvicentecv@gmail.com>
2025-04-19 17:18:44 -0400

    new function to assign role on users

--- app/Services/RoleHasPermissionsService.php
+++ app/Services/RoleHasPermissionsService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\Role_Has_Permissions;
+use App\Models\Role;
 
 class RoleHasPermissionsService
 {
@@ -11,5 +12,18 @@ class RoleHasPermissionsService
         return Role_Has_Permissions::all();
     }
     
-    //permisos por id permisos
+    public function userHasRole($userId){
+        return Role_Has_Permissions::where('user_id', $userId)->exists();
+    }
+
+    public function getAllRoles(){
+        return Role::all();
+    }
+
+    public function assignRole($userId, $roleId){
+        return Role_Has_Permissions::create([
+            'user_id' => $userId,
+            'role_id' => $roleId
+        ]);
+    }
 }
